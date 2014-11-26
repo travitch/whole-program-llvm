b8603e9399bca9f39ce3f67b8944b607e49e6aec
Ian A. Mason <iam@csl.sri.com>
2014-11-26 10:53:04 -0800

    Adding a dir for seeing if we can get it to work an a mac.

---
+++ darwin/baz.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+void baz(void){
+  fprintf(stderr, "baz\n");
+}
---
+++ darwin/main.c
@@ -0,0 +1,11 @@
+
+extern void foo(void);
+extern void bar(void);
+extern void baz(void);
+
+int main(int argc, char** argv){
+  foo();
+  bar();
+  baz();
+  return 0;
+}
