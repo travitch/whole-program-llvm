ea35ceecadcc1592a23a3f57d3f7eb87fb352b90
Ian A. Mason <iam@csl.sri.com>
2015-03-14 14:13:17 -0700

    First travis tests: build apache using llvm-3.4 and python 2.7 and 3.3

--- .travis/apache.sh
+++ .travis/apache.sh
@@ -2,3 +2,28 @@
 # Make sure we exit if there is a failure
 set -e
 
+
+export PATH=/usr/lib/llvm-3.4/bin:${WLLVM_HOME}:${PATH}
+export LLVM_COMPILER='clang'
+export WLLVM_OUTPUT=WARNING
+
+wget http://apache.mirrors.pair.com//httpd/httpd-2.4.12.tar.gz
+tar xfz httpd-2.4.12.tar.gz
+cd httpd-2.4.12
+CC=wwlvm ./configure
+make
+extract-bc httpd
+
+
+if [ -s "httpd.bc" ]
+then
+    echo "httpd.bc built."
+else
+    exit 1
+fi
+
+
+
+
+
+
