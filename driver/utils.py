193a5c549eaced33a7d1135829ab018c4c5847c4
Marko Dimjašević <marko@cs.utah.edu>
2016-06-16 17:26:25 -0600

    This is a cherry-pick of Ian A. Mason's commit to a fork at: https://github.com/SRI-CSL/whole-program-llvm/commit/17f2e850defb10f2a0eb3263041e618793125870
    It partially addresses the issue #39 by adding support for shared
    libraries. Other metioned flags stay unsupported with this commit.

--- driver/utils.py
+++ driver/utils.py
@@ -226,6 +226,9 @@ class ArgumentListFilter(object):
             #iam: the object file recogition is not really very robust, object files
             # should be determined by their existance and contents...
             r'^.+\.(o|lo|So|so|po|a)$' : (0, ArgumentListFilter.objectFileCallback),
+            #iam: library.so.4.5.6 probably need a similar pattern for .dylib too.
+            r'^.+\.dylib(\.\d)+$' : (0, ArgumentListFilter.objectFileCallback),
+            r'^.+\.(So|so)(\.\d)+$' : (0, ArgumentListFilter.objectFileCallback),
             r'^-(l|L).+$' : (0, ArgumentListFilter.linkUnaryCallback),
             r'^-I.+$' : (0, ArgumentListFilter.compileUnaryCallback),
             r'^-D.+$' : (0, ArgumentListFilter.compileUnaryCallback),
