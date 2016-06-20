c49a5caae6156194de7295749344cbd7c502b69f
Tristan Ravitch <tristan@nochair.net>
2016-06-20 07:09:07 -0700

    Update the apache source in the travis config
    It seems to be responsible for many test failures

--- .travis/apache_clang.sh
+++ .travis/apache_clang.sh
@@ -7,9 +7,9 @@ export PATH=/usr/lib/llvm-3.4/bin:${WLLVM_HOME}:${PATH}
 export LLVM_COMPILER=clang
 export WLLVM_OUTPUT=WARNING
 
-APACHE_VER=2.4.17
+APACHE_VER=2.4.18
+wget https://archive.apache.org/dist/httpd/httpd-${APACHE_VER}.tar.gz
 
-wget http://apache.mirrors.pair.com//httpd/httpd-${APACHE_VER}.tar.gz
 tar xfz httpd-${APACHE_VER}.tar.gz
 mv httpd-${APACHE_VER} apache_clang
 cd apache_clang
