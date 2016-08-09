c36b427753f00c6c8cb3d930df1d94b6c49217cf
Ian A. Mason <iam@csl.sri.com>
2016-08-09 16:36:58 -0700

    The import mystery remains. Re: as.py vs wllvm.py

--- wllvm/wllvmpp.py
+++ wllvm/wllvmpp.py
@@ -7,13 +7,11 @@ records the location of the bitcode in an ELF section of the object
 file so that it can be found later after all of the objects are linked
 into a library or executable.
 """
-from __future__ import absolute_import
-
 import sys, os
 
-from .utils import getBuilder, buildObject, buildAndAttachBitcode, logging
+from utils import getBuilder, buildObject, buildAndAttachBitcode, logging
 
-import .logconfig
+import logconfig
 
 _logger = logging.getLogger(__name__)
 
