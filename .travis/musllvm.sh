e85a9918fd62e133dfc74877925359b8d1c9fdbd
Ian A. Mason <iam@csl.sri.com>
2017-04-25 19:04:55 -0700

    testing the bitcode store in travis too.

--- .travis/musllvm.sh
+++ .travis/musllvm.sh
@@ -9,6 +9,9 @@ export WLLVM_OUTPUT=WARNING
 
 wllvm-sanity-checker
 
+#setup the store so we test that feature as well
+export WLLVM_BC_STORE=/tmp/bc
+mkdir /tmp/bc
 
 git clone https://github.com/SRI-CSL/musllvm.git musllvm
 cd musllvm
@@ -18,10 +21,23 @@ extract-bc --bitcode ./lib/libc.a
 
 if [ -s "./lib/libc.a.bc" ]
 then
-    echo "libc.a.bc."
+    echo "libc.a.bc exists."
 else
     exit 1
 fi
+
+#now lets makes sure the store has the bitcode too.
+mv ./lib/libc.a .
+make clean
+extract-bc --bitcode ./libc.a
+
+if [ -s "./libc.a.bc" ]
+then
+    echo "libc.a.bc exists."
+else
+    exit 1
+fi
+
 
 
 
