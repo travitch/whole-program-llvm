e4fc341f8a864b9b8c4155bd7bc8399a4efd10ee
Ian A. Mason <iam@csl.sri.com>
2019-06-18 11:36:32 -0700

    version bump

--- wllvm/version.py
+++ wllvm/version.py
@@ -75,7 +75,10 @@ Version History:
 1.2.5    - 4/17/2019 Fixing the pip package, hopefully.
 
 1.2.6    - 6/18/2019 Various compiler cmd line options parsing tweaks."
+
+1.2.7    - 6/18/2019 The File type fix."
+
 """
 
-wllvm_version = '1.2.6'
+wllvm_version = '1.2.7'
 wllvm_date = 'June 18 2019'
